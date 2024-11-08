e4e5ed47f08dc80ae0ebf76cf0132e738dd61f4b
Sarai Rojas <sara.r.sandoval@gmail.com>
2024-11-08 08:00:28 -0600

    refactor: replace wildcard import with single imports

--- src/main/java/com/course/fleet/management/api/controller/UserController.java
+++ src/main/java/com/course/fleet/management/api/controller/UserController.java
@@ -1,14 +1,23 @@
 package com.course.fleet.management.api.controller;
 
 import com.course.fleet.management.api.entity.User;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import net.datafaker.Faker;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @RestController
 @RequestMapping("/user")
