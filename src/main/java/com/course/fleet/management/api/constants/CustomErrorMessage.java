81778a8c0db65af121e0c54ec651a2afedb4f553
Sarai Rojas <sara.r.sandoval@gmail.com>
2024-11-14 17:58:13 -0600

    fix: change custom message for email already used

--- src/main/java/com/course/fleet/management/api/constants/CustomErrorMessage.java
+++ src/main/java/com/course/fleet/management/api/constants/CustomErrorMessage.java
@@ -5,7 +5,8 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class CustomErrorMessage {
 
-  public static final String USER_ALREADY_EXISTS = "The user already exists";
+  public static final String USER_ALREADY_EXISTS =
+      "The email address is already used by an existing user";
   public static final String USER_NOT_FOUND = "The user has not been found";
   public static final String BAD_REQUEST = "The given resource is malformed";
 
